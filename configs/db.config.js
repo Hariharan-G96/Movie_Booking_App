722a1de6e12dc5b5db9e5c62dea2a7b221bbea27
Hariharan <104617937+Hariharan-G96@users.noreply.github.com>
2023-02-14 21:16:52 +0530

    DB Changed

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME : 'movie_booking_db',
-    DB_URL : 'mongodb+srv://Hariharan:Hari@2496@cluster0.zjksf8o.mongodb.net/?retryWrites=true&w=majority'
+    DB_URL : 'mongodb://localhost:27017'
 }
